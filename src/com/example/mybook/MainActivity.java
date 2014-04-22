c2eb5ffe026f3bde8078b99e13bcea732aee42fb
efix0 <efixzero@googlemail.com>
2014-04-22 19:25:44 +0200

    -scanner

---
+++ src/com/example/mybook/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mybook;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity{
+
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		Button searchButton = (Button)findViewById( R.id.startActivity_Search );
+		searchButton.setOnClickListener( new View.OnClickListener(){
+			
+			@Override
+        	public void onClick(View v){
+				Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+				startActivity(i);
+			}
+		});
+
+	}
+	
+}
